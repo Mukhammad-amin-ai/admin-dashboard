31ce6d4bb432246b3921144ebb7c64475b305384
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-01-19 21:20:28 +0500

    changes

---
+++ src/components/dashboard-card.vue
@@ -0,0 +1,126 @@
+<template >
+    <div class="wrap">
+        <div class="card">
+            <div class="card-top">
+                <div class="left">
+                    <div class="icon">
+                        <img src="../assets/icons/Customer-Icon.svg" alt="Customer">
+                    </div>
+                    <h4>Customers</h4>
+                </div>
+                <div class="right">
+                    <div class="stat">
+                        <img src="../assets/icons/grow.svg" alt="grow">
+                        <span>16,5%</span> +1,293 month
+                    </div>
+                </div>
+            </div>
+            <div class="card-bottom">
+                <div class="box">
+                    <h4>20,6K</h4>
+                    <p>Active</p>
+                </div>
+                <div class="box">
+                    <h4>20,6K</h4>
+                    <p>Active</p>
+                </div>
+                <div class="box">
+                    <h4>20,6K</h4>
+                    <p>Active</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+
+</script>
+<style scoped>
+.wrap {
+    width: 321px;
+    height: 196px;
+    border-radius: 8px;
+    background: #FFF;
+}
+
+.card {
+    width: 100%;
+    height: 100%;
+    padding: 16px;
+}
+
+.card-top {
+    width: 100%;
+    height: 50%;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #E0E1E5;
+}
+
+.left {
+    width: 40%;
+    height: 100%;
+}
+
+.left h4 {
+    font-size: 16px;
+    color: #1C1F27;
+    font-weight: 500;
+    font-family: Euclid-Medium;
+}
+
+.right {
+    width: 60%;
+    height: 100%;
+    display: flex;
+    justify-content: end;
+    align-items: flex-start;
+   
+}
+.stat{
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    color: #92969F;
+    font-size: 14px;
+    gap: 5px;
+    font-family: Euclid-Regular;
+}
+
+.stat span{
+    color: #4478FF;
+    font-weight: 600;
+}
+.card-bottom{
+    width: 100%;
+    height: 50%;
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-between;
+}
+.box{
+    width: 88px;
+    height: 56px;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    justify-content: center;
+}
+.box h4{
+    font-size: 24px;
+    font-weight: 500;
+    font-family: Euclid-Regular;
+}
+
+.box:nth-child(1) h4{
+    font-weight: 600;
+}
+
+.box p{
+    color: #92969F;
+    font-size: 14px;
+    font-weight: 500;
+    font-family: Euclid-Regular;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,270 @@
 <template>
- <div class="container">
-    
- </div>
+  <div class="container">
+    <div class="header-section">
+      <div class="navigation">
+        <button class="arrow-left">
+          <img src="../assets/icons/arrow-left.svg" alt="arrow-left">
+        </button>
+        <div class="nav-bar">
+          <div class="logo">
+            <img src="../assets/svg/logo.svg" alt="logo">
+          </div>
+          <div class="config">
+            <div class="show">
+              Showing:
+              <select>
+                <option value="Week">Weekly</option>
+                <option value="Month">Monthly</option>
+                <option value="Year">Yearly</option>
+              </select>
+            </div>
+
+            <div class="docs">
+              Export Data
+              <img src="../assets/icons/document-download.svg" alt="document-download">
+            </div>
+            <div class="search">
+              <img src="../assets/icons/search-normal.svg" alt="search-normal">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="breadcrumb">
+          Homepage / <span>Admin Panel</span>
+        </div>
+        <div class="hero-text">
+          <h1>Hello, Farooq 👋🏻</h1>
+          <p>Lorem ipsum dolor sit amet consectetur. Purus in scelerisque ut felis sit et a proin nulla.</p>
+        </div>
+        <div class="tab-bar">
+          <div class="item active">General</div>
+          <div class="item">Customer Management</div>
+          <div class="item">Invoicing</div>
+        </div>
+      </div>
+    </div>
+    <div class="main-section">
+      <div class="dashboard-wrapper">
+        <div class="dashboard-cards">
+          <DashboardCards />
+          <DashboardCards />
+          <DashboardCards />
+          <DashboardCards />
+        </div>
+      </div>
+      f
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import DashboardCards from '../components/dashboard-card.vue'
+
 
 </script>
 <style scoped>
-.container{
+.container {
   width: 100vw;
   max-width: 100%;
   height: auto;
 }
 
+.header-section {
+  width: 100%;
+  height: 544px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: rgba(25, 32, 56, 1);
+}
 
+.navigation {
+  width: 100%;
+  height: 81px;
+  display: flex;
+  align-items: center;
+  margin-top: 23.09px;
+  gap: 8px;
+}
+
+.arrow-left {
+  background-color: transparent;
+  border: none;
+  margin-left: 16px;
+  cursor: pointer;
+}
+
+.nav-bar {
+  width: 93%;
+  height: auto;
+  padding: 20.457px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #344159;
+}
+
+.logo {
+  width: auto;
+  height: auto;
+
+}
+
+.config {
+  width: 375px;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.show {
+  display: inline-flex;
+  height: 40px;
+  padding: 14px;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 8px;
+  flex-shrink: 0;
+  border-radius: 8px;
+  border: 1px solid rgba(105, 110, 130, 1);
+  color: #92969F;
+  font-size: 12px;
+  font-family: Euclid-Medium;
+}
+
+.show select {
+  border: none;
+  background-color: transparent;
+  color: #FFF;
+  font-size: 12px;
+  font-family: Euclid-Medium;
+}
+
+.show select:focus {
+  outline: none;
+}
+
+.show select option {
+  color: #000;
+}
+
+
+.docs {
+  cursor: pointer;
+  display: inline-flex;
+  height: 40px;
+  padding: 14px;
+  align-items: center;
+  gap: 8px;
+  border-radius: 8.183px;
+  border: 1.023px solid rgba(105, 110, 130, 1);
+  color: #FFF;
+  font-size: 12px;
+  font-family: Euclid-Medium;
+  line-height: 16px;
+}
+
+.search {
+  cursor: pointer;
+  display: flex;
+  padding: 10.228px;
+  align-items: flex-start;
+  gap: 8.183px;
+  border-radius: 8.183px;
+  border: 1.023px solid rgba(105, 110, 130, 1);
+}
+
+.wrapper {
+  width: 94%;
+  height: auto;
+}
+
+.breadcrumb {
+  margin-top: 47px;
+  color: #696E82;
+  font-size: 14px;
+  font-family: Euclid-Medium;
+}
+
+.breadcrumb span {
+  color: #fff;
+}
+
+.hero-text {
+  margin-top: 24px;
+}
+
+.hero-text h1 {
+  font-size: 48px;
+  font-family: Euclid-Medium;
+  font-weight: 500;
+  color: #fff;
+}
+
+.hero-text p {
+  width: 427px;
+  color: #ABB0C7;
+  font-size: 14px;
+  font-family: Euclid-Regular;
+  font-style: normal;
+  font-weight: 400;
+}
+
+.tab-bar {
+  margin-top: 33px;
+  display: inline-flex;
+  padding: 6px;
+  align-items: flex-start;
+  gap: 8px;
+  border-radius: 12px;
+  border: 1px solid #696E82;
+  background: #161A2A;
+}
+
+.item {
+  display: flex;
+  padding: 6px 12px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  border-radius: 6px;
+  color: #fff;
+  font-size: 14px;
+  font-family: Euclid-Medium;
+  cursor: pointer;
+}
+
+.active {
+  background: #4478FF;
+}
+
+.dashboard-wrapper {
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  bottom: 100px;
+}
+
+.dashboard-cards {
+  width: 93%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 64px;
+  gap: 20px;
+}
+
+
+.main-section {
+  width: 100%;
+  height: 1758px;
+  background-color: #F8F8F8;
+ 
+}
 </style>
